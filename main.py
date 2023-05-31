31fbb07077d9cb9a8577a608b86d8e96c30cc5cd
oscar7845 <oc59@njit.edu>
2023-05-31 17:17:22 +0200

    on change

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ def main(page: ft.Page):
     page.window_width = page.window_width if page.window_width is not None else page.window_min_width
     page.window_height = page.window_height if page.window_height is not None else 820.0
 
+    # on change
+    def text_background_method():
+        page.update()
+
+    def keep_window_above_method():
+        pass
+
+    def night_theme_method():
+        page.update()
+    
+
+    def font_size_method():
+        pass
+
+
 
 if __name__ == "__main__":
     ft.app(target=main)
