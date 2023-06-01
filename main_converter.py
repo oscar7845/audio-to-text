b6230997db9d09272ec90d66d0cfd13999705276
oscar7845 <oc59@njit.edu>
2023-06-01 16:14:18 +0200

    last

--- main_converter.py
+++ main_converter.py
@@ -1,6 +1,12 @@
 import flet as ft
+import io, os, yaml
 
 def main(page: ft.Page):  
+
+    PEAK_POW = 5000
+    AUDIO_SAMPLING_FREQ = 16000
+    FPB = 1024
+    LARGEST_SHORT_INT16 = 32767 
 
     # Setting up the window appearence  
     page.title = "Audio to Text App"
@@ -10,8 +16,11 @@ def main(page: ft.Page):
     page.window_height = page.window_height if page.window_height is not None else 820.0
 
     # on change
-    def text_background_method():
-        page.update()
+    def keep_window_above_method(_):
+       page.window_always_on_top = keep_above_box.value
+       page.update()
+
+    keep_above_box = ft.Checkbox(label="Keep Above", value=params.get('keep_above', False), on_change=keep_window_above_method)
 
     def keep_window_above_method():
         pass
@@ -23,6 +32,11 @@ def main(page: ft.Page):
     def font_size_method():
         pass
 
+    # Save all the params
+    params = {
+        'window_width': page.window_width,
+        'window_height': page.window_height
+    }
 
     model_dropdown = ft.Dropdown(
             options=[
@@ -46,6 +60,7 @@ def main(page: ft.Page):
 
     page.add(
         settings_controls,
+        keep_above_box
         #
     )
 
