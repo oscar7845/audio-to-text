fbd6ccf4abad2d3639eaf9b0dd6e56e7104b67b6
oscar7845 <oc59@njit.edu>
2023-06-01 16:33:10 +0200

    last edi

--- main_converter.py
+++ main_converter.py
@@ -1,8 +1,8 @@
 import flet as ft
 import io, os, yaml
+import pyaudio
 
 def main(page: ft.Page):  
-
     PEAK_POW = 5000
     AUDIO_SAMPLING_FREQ = 16000
     FPB = 1024
@@ -10,8 +10,8 @@ def main(page: ft.Page):
 
     # Setting up the window appearence  
     page.title = "Audio to Text App"
-    page.window_min_width = 840.0
-    page.window_min_height = 480.0
+    page.window_min_width = 750.0
+    page.window_min_height = 475.0
     page.window_width = page.window_width if page.window_width is not None else page.window_min_width
     page.window_height = page.window_height if page.window_height is not None else 820.0
 
@@ -68,30 +68,82 @@ def main(page: ft.Page):
         'window_height': page.window_height
     }
 
-    model_dropdown = ft.Dropdown(
-            options=[
-            ],
-            label="Speech To Text Model",
-            expand=True,
-        )
-    
-    settings_controls = ft.Column(
+    model_selector = ft.Dropdown(
+        options=[
+            ft.dropdown.Option('tiny', text="Smallest"),
+            ft.dropdown.Option('base', text="Default"),
+            ft.dropdown.Option('small', text="Small"),
+            ft.dropdown.Option('medium', text="Medium"),
+            ft.dropdown.Option('large', text="Large"),
+        ],
+        label="Selected Audio/Speech to Text Type",
+        expand=True,
+        text_size=15,
+    )
+
+    mics = {}
+    paudio = pyaudio.PyAudio()
+    for i in range(paudio.get_device_count()):
+        device_details = paudio.get_device_info_by_index(i)
+        if device_details['maxInputChannels'] > 0 and device_details['hostApi'] == 0:
+            mics[device_details['index']] = device_details['name']
+
+    default_microphone = paudio.get_default_input_device_info()['index']
+    selected_microphone = int(params.get('microphone_index', default_microphone))
+    if selected_microphone not in mics:
+        selected_microphone = default_microphone
+
+    mic_selector = ft.Dropdown(
+        options=[ft.dropdown.Option(index, text=mic) for index, mic in mics.items()],
+        label="Selected Audio Input Device",
+        expand=True,
+        content_padding=ft.padding.only(top=5, bottom=5, left=10),
+        text_size=15,
+    )
+
+    params_config = ft.Column(
     [
         ft.Container(
             content=ft.Row(
                 [
-                    model_dropdown,                
+                    model_selector,
                 ],
             ),
+            padding=ft.padding.symmetric(horizontal=10, vertical=10),
+        ),
+        ft.Container(
+            content=ft.Row(
+                [
+                    mic_selector,
+                ],
+            ),
+            padding=ft.padding.symmetric(horizontal=10, vertical=10),
+        ),
+
+        ft.Container(
+            content=ft.Row(
+                [
+                    ft.Row(
+                        [
+                            lang_selector,
+                            text_size_selector,
+                        ]
+                    ),
+                ],
+            ),
+            padding=ft.padding.symmetric(horizontal=10, vertical=10),
         ),
     ],
     visible=True
-)
+    )
+
+    #--#
 
     page.add(
-        settings_controls,
-        keep_above_box
-        #
+        params_config,
+        ft.Container(
+            content=convertion_lst,
+        ),
     )
 
 
